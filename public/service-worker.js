51d87ba835706b620e4f59f3dc57dd43d68d7349
arieltherapy <ginatnoy@gmail.com>
2025-09-18 21:59:22 +0300

    Create service-worker.js

---
+++ public/service-worker.js
@@ -0,0 +1,27 @@
+const CACHE_NAME = 'app-v1';
+const ASSETS = [
+  '/',
+  '/index.html'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(k => k !== CACHE_NAME && caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
+});
